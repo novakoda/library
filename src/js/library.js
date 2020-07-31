4315d152d7e6fd84ceaa5371ddf911a21304ab4e
JR Nova <novakoda9@gmail.com>
2020-07-31 13:41:40 -0500

    can delete books

--- src/js/library.js
+++ src/js/library.js
@@ -1,4 +1,5 @@
 var myLibrary = [];
+var libCont = document.getElementById('myLibrary');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -11,9 +12,9 @@ function Book(title, author, pages, read) {
   if (this.read == true) {
     this.readStatus = "read";
   }
-
   this.info = `${title} by ${author}, ${pages} pages - ${this.readStatus}`;
 }
+
 
 function addBookToLibrary(title, author, pages, read) {
   book = new Book(title, author, pages, read);
@@ -22,22 +23,28 @@ function addBookToLibrary(title, author, pages, read) {
   listBooks();
 }
 
-function render(html) {
-  document.getElementById('myLibrary').innerHTML = html;
-}
 
 function listBooks() {
   var html = ""
-  myLibrary.forEach((book) => {
-    html += `<tr><th scope="row">${book.title}</th><td>${book.author}</td><td>${book.pages}</td><td>${book.readStatus}</td></tr>`
+  myLibrary.forEach((book, i) => {
+    html += `<tr id="book${i}"><th scope="row">${book.title}</th><td>${book.author}</td><td>${book.pages}</td><td>${book.readStatus}</td><td><button id="delete${i}" class="btn btn-danger px-1 py-0">X</button></td></tr>`
   });
+  libCont.innerHTML = html;
 
-  render(html);
+  for (let i = 0; i < myLibrary.length; i++) {
+    window["book"+i] = document.getElementById(`book${i}`);
+
+    document.getElementById(`delete${i}`).addEventListener("click",  function() {
+      myLibrary.splice(i, 1);
+      libCont.removeChild(window["book"+i]);
+      listBooks();
+    });
+  }
 }
+
 
 function formData(form) {
   var messages = [];
-
   title = document.getElementById("formTitle").value;
   author = document.getElementById("formAuthor").value;
   pages = document.getElementById("formPages").value;
@@ -45,11 +52,9 @@ function formData(form) {
   if (title === "" || title == null) {
     messages.push("Title cannot be empty");
   }
-
   if (author === "" || author == null) {
     messages.push("Author cannot be empty");
   }
-
   if (pages === "" || pages == null) {
     messages.push("Pages cannot be empty");
   }
@@ -61,16 +66,17 @@ function formData(form) {
     return;
   }
   error.style.display = "none";
-  
+
   if (document.getElementById("formRead").checked) {
     read = true;
   } else {
     read = false;
-  };
-
+  }
   addBookToLibrary(title, author, pages, read);
   document.querySelector("#book-form-container").style.display = "none";
 }
+
+
 
 addBookToLibrary("Think and Grow Rich", "Napoleon Hill", 238, false);
 addBookToLibrary("Outwitting the Devil", "Napoleon Hill", 288, true);
